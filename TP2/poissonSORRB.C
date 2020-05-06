d5b8a9e00d8847c740b9a0e3eaacb76af0737693
47Balboa <shahzod1997@hotmail.com>
2020-05-06 17:27:36 +0100

    erro

--- TP2/poissonSORRB.C
+++ TP2/poissonSORRB.C
@@ -3,7 +3,11 @@
 #include <stdlib.h>
 #include <math.h>
 
+
+#define M_PI 3.14159265358979323846264338327950288 
+
 float** w;
+float** u;
 
 //número de pontos da grelha
 int N;
@@ -13,6 +17,16 @@ void initialize_matrices(){
     int i, j;
     
     w = (float**) malloc(sizeof(float*) * N);
+    u = (float**) malloc(sizeof(float*) * N);
+
+    //inicializar a matriz u a zero
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            u[i][j]=0;
+            printf("%f",u[i][j]);
+        }
+        
+    }
     
     // Preencher a matriz com 100 nos limites superior e laterais e 50 nos interiores
         for(i = 0; i < N-1; i++){
@@ -21,17 +35,17 @@ void initialize_matrices(){
 
             for(j = 0; j < N; j++){ 
             
-                if(i == 0 || j == 0 || j == N-1) //fronteiras superior e laterais
+                if(i == 0 || j == 0 || j == N-1) //fronteiras inferiores e laterais
 
                     w[i][j] = 100;
                 
-                 else  w[i][j] = 50; //valores iniciais dos pontos interiores   
+                 else  w[i][j] = 50; //valores iniciais dos pontos superiores  
            }
     }
 
     w[i] = (float*) malloc(sizeof(float) * N);
     
-    // fronteira de baixo preenchida a 0;
+    // fronteira de cima preenchida a 0;
     for(j = 0; j < N; j++){ 
       
         w[i][j] = 0;
@@ -56,6 +70,48 @@ void free_matrices(){
     free(w);
 }
 
+//funcao que calcula a diferenca entre 2 vetores e guarda o resultado noutro vetor
+float** diferenca(float** a, float** b, int N){
+    float** result = (float**)malloc(N* sizeof(float));
+   
+    for(int i=0;i<N;i++){
+        result[i] = (float*)malloc(N*sizeof(float*));
+    }
+
+    for(int i= 0; i<N; i++){
+        for(int j=0;j<N; j++){
+            result[i][j] = 0;
+        }
+    }
+
+    for(int i=0; i<N; i++){
+        for(int j=0;j<N; j++){
+            result[i][j] = a[i][j] - b[i][j];
+        }
+    }
+    return result;
+}
+
+//funcao que retorna um vetor bi-dimensional com o valor absoluto de cada elemento
+float** absol(float** vetor, int N){
+    for(int i;i<N; i++){
+        for(int j=0;j<N; j++){
+            vetor[i][j] = fabs(vetor[i][j]);
+        }
+    }
+    return vetor;
+}
+
+//funcao que retorna o maior elemento de um vetor
+float maximum(float** vetor, int N){
+    float max = 0;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            if(vetor[i][j]>max) max= vetor[i][j];
+        }
+    }
+    return max;
+}
 
 int main(int argc, char* argv[]){
 
@@ -74,8 +130,8 @@ int main(int argc, char* argv[]){
     // parâmetro de relaxamento
     p=2/(1+sin(M_PI/(N-1)));
 
-    // COMO É QUE SEI O VALOR INICIAL de tol
-    float tol;
+    
+    float tol = 1/ (N^2);
     float diff = tol + 1;
 
     printf("N: %d,P: %d, TOL: %f, DIFF: %f\n", N,p,tol, diff);
@@ -83,19 +139,19 @@ int main(int argc, char* argv[]){
     iter = 0;    
     
     while(diff > tol){
-        //cópia da ultima iteração. MAS O QUE É O u?
-        //u=w 
+        
+        u=w ;
       printf("olaaa");
         // 
-        for(i = 2; i < N-1; i++){
-            for(j= 2+(i%2); j < N-1; j++){
+        for(i = 1; i < N-1; i++){
+            for(j= 1+ (i%2); j < N-1; j+=2){
                 w[i][j]=(1-p)* w[i][j]+p*(w[i-1][j]+w[i][j-1]+w[i][j+1]+w[i+1][j])/4;
             }
         }
 
-        for(i = 2; i < N-1; i++){
+        for(i = 1; i < N-1; i++){
             
-            for(j= 2+((i+1)%2); j < N-1; j+=2){
+            for(j= 1+((i+1)%2); j < N-1; j+=2){
                  w[i][j]=(1-p)* w[i][j]+p*(w[i-1][j]+w[i][j-1]+w[i][j+1]+w[i+1][j])/4;
             }
         }
@@ -103,9 +159,13 @@ int main(int argc, char* argv[]){
         // Aumentar o número de iterações
         iter++;
 
-        // Buscar o maior valor do máximo absoluto da diferença entre w-u
+        // Buscar ,em valor absoluto, o máximo da diferença das componentes em 2 iterações sucessivas
+        //Em matlab o 1ºmax retorna um vetor linha com os maximos de cada linha da "matrix" e o segundo max retorna o valor maximo desse vetor
         //diff=max(max(abs(w-u)));
+        diff=maximum(absol(diferenca(w,u,N),N),N);
     }
+
+
     
     print_matrix();
 
