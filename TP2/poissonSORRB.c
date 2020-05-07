5925af4ae814665d6b8c6e556eb1414f76be5ea5
AdrianaMeireles80 <adriana_1998@live.com.pt>
2020-05-07 01:43:15 +0100

    olé

--- TP2/poissonSORRB.c
+++ TP2/poissonSORRB.c
@@ -4,7 +4,7 @@
 #include <math.h>
 
 
-#define M_PI 3.14159265358979323846264338327950288 
+//#define M_PI 3.14159265358979323846264338327950288 
 
 float** w;
 float** u;
@@ -74,7 +74,7 @@ void free_matrices(){
 }
 
 //funcao que calcula a diferenca entre 2 vetores e guarda o resultado noutro vetor
-float** diferenca(float** a, float** b, int N){
+float** diferenca(float** a, float** b){
     float** result = (float**)malloc(N* sizeof(float));
    
     for(int i=0;i<N;i++){
@@ -96,7 +96,7 @@ float** diferenca(float** a, float** b, int N){
 }
 
 //funcao que retorna um vetor bi-dimensional com o valor absoluto de cada elemento
-float** absol(float** vetor, int N){
+float** absol(float** vetor){
     for(int i;i<N; i++){
         for(int j=0;j<N; j++){
             vetor[i][j] = fabs(vetor[i][j]);
@@ -106,7 +106,7 @@ float** absol(float** vetor, int N){
 }
 
 //funcao que retorna o maior elemento de um vetor
-float maximum(float** vetor, int N){
+float maximum(float** vetor){
     float max = 0;
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
@@ -116,7 +116,7 @@ float maximum(float** vetor, int N){
     return max;
 }
 
-void iguala(float **a, float**b,int N){
+void iguala(float **a, float**b){
     int i,j;
 
     for(i =0;i<N;i++){
@@ -131,32 +131,50 @@ int main(int argc, char* argv[]){
     if(argc != 2){
         
         printf ("Usage : ./ poissonSORRB <nr pontos da grelha>\n");
-         _exit(0);
+        
     }
 
-    int N = atoi(argv[1]);
-    int i, j, p , iter;
+    N = atoi(argv[1]);
+    
+   // printf("N: %d", N);
 
+    int i, j;
+
+   
     // Preparar as matrizes para aplicar o algoritmo
     initialize_matrices();
     
     print_matrix();
     // parâmetro de relaxamento
-    p=2/(1+sin(M_PI/(N-1)));
+    float p=2/(1+sin(M_PI/(N-1)));
+
+   // printf("p: %f", p);
 
     
-    float tol = 1/ (N^2);
-    float diff = tol + 1;
+    float tol = 1/ (float)(N*N);
+    //printf("TOL: %f", tol);
 
-    printf("N: %d,P: %d, TOL: %f, DIFF: %f\n", N,p,tol, diff);
 
-    iter = 0;    
+    float diff = (tol + 1);
+    //printf("DIFF: %f", diff);
+
+    printf("sou linda\n");
+
+    printf("N: %d,P: %f, TOL: %f, DIFF: %f\n", N,p,tol, diff);
+
+    printf("porque\n");
+
+     printf("looooooool\n");
+   
+   int iter = 0;
+
+   printf("Número de Iterações: %d\n", iter);
     
     while(diff > tol){
-        iguala(u,w,N);
-        
         printf("olaaa");
-        // 
+        iguala(u,w);
+        
+       
         for(i = 1; i < N-1; i++){
             for(j= 1+ (i%2); j < N-1; j+=2){
                 w[i][j]=(1-p)* w[i][j]+p*(w[i-1][j]+w[i][j-1]+w[i][j+1]+w[i+1][j])/4;
@@ -176,12 +194,8 @@ int main(int argc, char* argv[]){
         // Buscar ,em valor absoluto, o máximo da diferença das componentes em 2 iterações sucessivas
         //Em matlab o 1ºmax retorna um vetor linha com os maximos de cada linha da "matrix" e o segundo max retorna o valor maximo desse vetor
         //diff=max(max(abs(w-u)));
-        diff=maximum(absol(diferenca(w,u,N),N),N);
+        diff=maximum(absol(diferenca(w,u)));
     }
-
-
-    
-    print_matrix();
 
     printf("Número de Iterações: %d\n", iter);
 
